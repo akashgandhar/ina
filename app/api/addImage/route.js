24cfb9ae5838586309debece411605e87e463d83
Akash Gandhar <76213112+akashgandhar@users.noreply.github.com>
2025-04-20 22:54:24 +0530

    dfghuio

--- app/api/addImage/route.js
+++ app/api/addImage/route.js
@@ -1,30 +1,30 @@
-// add an product image
+// // add an product image
 
-import { createAdminRestApiClient } from "@shopify/admin-api-client";
-import { NextResponse } from "next/server";
+// import { createAdminRestApiClient } from "@shopify/admin-api-client";
+// import { NextResponse } from "next/server";
 
-export async function POST(request) {
-  const { image, productId } = await request.json();
-  console.log(image);
-  console.log(productId);
+// export async function POST(request) {
+//   const { image, productId } = await request.json();
+//   console.log(image);
+//   console.log(productId);
 
-  const client = createAdminRestApiClient({
-    storeDomain: "https://52a54f-dd.myshopify.com",
-    apiVersion: "2025-04",
+//   const client = createAdminRestApiClient({
+//     storeDomain: "https://52a54f-dd.myshopify.com",
+//     apiVersion: "2025-04",
     
-  });
+//   });
 
-  const payload = {
-    image: {
-      src: image,
-      product_id: productId,
-    },
-  };
+//   const payload = {
+//     image: {
+//       src: image,
+//       product_id: productId,
+//     },
+//   };
 
-  const response = await client.post(`products/${productId}/images.json`, {
-    data: payload,
-  });
+//   const response = await client.post(`products/${productId}/images.json`, {
+//     data: payload,
+//   });
 
-  console.log(response);
-  return NextResponse.json(response);
-}
+//   console.log(response);
+//   return NextResponse.json(response);
+// }
